b19c09c40fc3f2502985530012fc2ce0e4b3f4c4
jspc <james@zero-internet.org.uk>
2016-03-19 17:04:02 +0000

    Gracefully handle errors and bad character sets

--- markov.rb
+++ markov.rb
@@ -17,14 +17,18 @@ redis_object = Redis.new(host: redis_host, port: redis_port)
 set_name = 'markov_tweets'
 
 loop do
-  t = redis_object.srandmember set_name
+  t = redis_object.srandmember(set_name).force_encoding('UTF-8')
 
   if t.nil?
     AnonRep::Helpers.log 'markov_tweeter', 'Nothing to tweet'
   else
-    tweet t
-    redis_object = s.srem set_name, t
-    AnonRep::Helpers.log 'markov_tweeter', "Tweeted '#{t}'"
+    begin
+      tweet t
+      redis_object = redis_object.srem set_name, t
+      AnonRep::Helpers.log 'markov_tweeter', "Tweeted '#{t}'"
+    rescue Exception => e
+      AnonRep::Helpers.log 'markov_tweeter', e.message
+    end
   end
 
   sleep_time = rand(120*60) # Two hours
